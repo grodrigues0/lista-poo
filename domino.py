33fe04b3f1d0f641a294139275e6a2e5807bbb56
Gustavo Rodrigues <trashypg@gmail.com>
2025-04-03 16:46:52 -0300

    :bug: fix: correção na função Domino

--- domino.py
+++ domino.py
@@ -1,7 +1,19 @@
+
 class Domino:
+    @property
     def __init__(self, esquerda: int, direita: int):
         self.esquerda = esquerda
         self.direita = direita
+
+    @__init__.setter
+    def __init__(self, lados: tuple):
+        self.esquerda = lados[0]
+        self.direita = lados[1]
+
+        if self.esquerda < 0 or self.direita < 0:
+            raise ValueError("Os lados do dominó devem ser não negativos.")
+        if self.esquerda > 6 or self.direita > 6:
+            raise ValueError("Os lados do dominó devem ser menores ou iguais a 6.")
 
     def mostrar_pontos(self):
         return f"Lado A: {self.esquerda} | Lado B: {self.direita}"
