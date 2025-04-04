4f6010e38101dc2dd460d389e711a3612c7ad67d
Gustavo Rodrigues <trashypg@gmail.com>
2025-04-04 08:20:08 -0300

    :bug: fix: classe Domino

--- domino.py
+++ domino.py
@@ -1,19 +1,28 @@
 
 class Domino:
-    @property
     def __init__(self, esquerda: int, direita: int):
         self.esquerda = esquerda
         self.direita = direita
 
-    @__init__.setter
-    def __init__(self, lados: tuple):
-        self.esquerda = lados[0]
-        self.direita = lados[1]
-
-        if self.esquerda < 0 or self.direita < 0:
-            raise ValueError("Os lados do dominó devem ser não negativos.")
-        if self.esquerda > 6 or self.direita > 6:
-            raise ValueError("Os lados do dominó devem ser menores ou iguais a 6.")
+    @property
+    def esquerda(self):
+        return self._esquerda
+    
+    @esquerda.setter
+    def esquerda(self, value):
+        if value < 0 or value > 6:
+            raise ValueError("Valor deve estar entre 0 e 6.")
+        self._esquerda = value
+    
+    @property
+    def direita(self):
+        return self._direita
+    
+    @direita.setter
+    def direita(self, value):
+        if value < 0 or value > 6:
+            raise ValueError("Valor deve estar entre 0 e 6.")
+        self._direita = value
 
     def mostrar_pontos(self):
         return f"Lado A: {self.esquerda} | Lado B: {self.direita}"
@@ -24,13 +33,14 @@ class Domino:
     def __str__(self):
         return f"Dominó ({self.esquerda}, {self.direita})"
     
+# -------------------- Testes --------------------
 
-d1 = Domino(2, 6)
-d2 = Domino(4, 3)
+# d1 = Domino(2, 6)
+# d2 = Domino(4, 3)
 
-print(d1.mostrar_pontos())
-print(d2.mostrar_pontos())
+# print(d1.mostrar_pontos())
+# print(d2.mostrar_pontos())
 
-print(f"Total de pontos: {d1.valor() + d2.valor()}")
+# print(f"Total de pontos: {d1.valor() + d2.valor()}")
 
-print(d1)
+# print(d1)
